ebbe596046a335c3c0779a40d629cebe8bc89c19
Hagen Papa Kodwo Dickson <hagendickson604@gmail.com>
2025-12-15 20:00:48 +0000

    Fix environment variable for database name

--- db_connect.py
+++ db_connect.py
@@ -7,7 +7,7 @@ def get_db_connection():
             host=os.environ.get('MYSQLHOST'),
             user=os.environ.get('MYSQLUSER'),
             password=os.environ.get('MYSQLPASSWORD'),
-            database=os.environ.get('MYSQLDATABASE'),
+            database=os.environ.get('MYSQL_DATABASE'),
             port=os.environ.get('MYSQLPORT')
         )
     else:
@@ -46,3 +46,4 @@ if __name__ == "__main__":
             if 'cursor' in locals():
                 cursor.close()
             close_connection(conn)
+
